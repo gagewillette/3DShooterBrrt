c0832355f3f155c53a2739d5a487bd07c9990379
gagewillette <gagew0513@gmail.com>
2023-08-17 13:02:20 -0400

    initial

---
+++ Assets/Scripts/PlayerMoveScript.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerMoveScript : MonoBehaviour
+{
+    //other
+    public Rigidbody rb;
+    public Transform orientation;
+    private float moveSpeed = 900f;
+    private float multiplier;
+    private float multiplierV;
+    
+    
+    //player bools
+    private float x, y;
+    private bool jump, crouching;
+
+    //physics
+    public bool grounded;
+
+    //crouch
+    private Vector3 crouchScale = new Vector3(1, 0.5f, 1);
+    private Vector3 playerScale;
+
+    public float jumpAmount = 3f;
+
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        rb = gameObject.GetComponent<Rigidbody>();
+        playerScale = transform.localScale;
+    }
+
+    public void FixedUpdate()
+    {
+        multiplier = grounded ? 1f : .5f;
+        multiplierV = multiplier;
+        Debug.Log(multiplier);
+        CheckInput();
+        Movement();
+    }
+
+    public void Update()
+    {
+        Debug.LogWarning(grounded);
+    }
+
+    private void CheckInput()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+        jump = Input.GetKey(KeyCode.Space);
+        crouching = Input.GetKey(KeyCode.LeftControl);
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            StartCrouch();
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            StopCrouch();
+        }
+    }
+
+    private void StartCrouch()
+    {
+        transform.localScale = crouchScale;
+        transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+    }
+
+    private void StopCrouch()
+    {
+        transform.localScale = playerScale;
+        transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+    }
+
+    private void Movement()
+    {
+        if (jump && grounded)
+        {
+            Debug.Log("Jump");
+            rb.AddForce(Vector3.up * jumpAmount, ForceMode.Impulse);
+        }
+            
+        rb.AddForce(orientation.transform.forward * y * moveSpeed * Time.deltaTime * multiplier * multiplierV);
+        rb.AddForce(orientation.transform.right * x * moveSpeed * Time.deltaTime * multiplier);
+        
+    }
+    
+
+    private void OnCollisionStay(Collision collision)
+    {
+        //check if collider is the ground layer
+        // if true then grounded`
+        tag = gameObject.tag;
+        if (tag.Equals("Player") && collision.collider.tag.Equals("Ground"))
+        {
+            grounded = true;
+            return;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        String tag = gameObject.tag;
+
+        if (tag.Equals("Player") && collision.collider.tag.Equals("Ground"))
+        {
+            grounded = false;
+        }
+    }
+}
